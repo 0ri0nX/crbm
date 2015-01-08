60430b462d76802c9f17fe3d93058fd187c31eb3
Hroza, Jiri <jiri.hroza@firma.seznam.cz>
2015-01-08 16:00:08 +0100

    linking and test

--- crbmComputer/crbmComputerTest.py
+++ crbmComputer/crbmComputerTest.py
@@ -1,17 +1,34 @@
+#!/usr/bin/env python
+import sys
 import crbmComputer as c
 from PIL import Image
 import numpy as n
 
 import time
 
-instance = c.CRBMComputer(["../dataConv/data5-5k-200x200x3.txt.rbm", "../dataConv/data5-5k-200x200x3.txt.transformed.rbm", "../dataConv/data5-5k-200x200x3.txt.transformed.transformed.rbm", "../dataConv/data5-5k-200x200x3.txt.transformed.transformed.transformed.rbm", "../dataConv/data5-5k-200x200x3.txt.transformed.transformed.transformed.transformed.rbm", "../dataConv/data5-5k-200x200x3.txt.transformed.transformed.transformed.transformed.transformed.rbm"], 1)
+#instance = c.CRBMComputer(["../dataConv/data5-5k-200x200x3.txt.rbm", "../dataConv/data5-5k-200x200x3.txt.transformed.rbm", "../dataConv/data5-5k-200x200x3.txt.transformed.transformed.rbm", "../dataConv/data5-5k-200x200x3.txt.transformed.transformed.transformed.rbm", "../dataConv/data5-5k-200x200x3.txt.transformed.transformed.transformed.transformed.rbm", "../dataConv/data5-5k-200x200x3.txt.transformed.transformed.transformed.transformed.transformed.rbm"], 1)
+
+if len(sys.argv) < 2:
+    print "Syntax:", sys.argv[0], "[image 1] ... [image N]"
+    exit(1)
+
+instance = c.CRBMComputer([
+        "testWeights/img-batch2.0.rbm",
+        "testWeights/img-batch2.0.transformed.rbm",
+        "testWeights/img-batch2.0.transformed.transformed.rbm",
+        "testWeights/img-batch2.0.transformed.transformed.transformed.rbm",
+        "testWeights/img-batch2.0.transformed.transformed.transformed.transformed.rbm",
+        "testWeights/img-batch2.0.transformed.transformed.transformed.transformed.transformed.rbm",
+        "testWeights/img-batch2.0.transformed.transformed.transformed.transformed.transformed.transformed.rbm",
+        #"testWeights/img-batch2.0.transformed.transformed.transformed.transformed.transformed.transformed.transformed.rbm"
+        ], 1)
 
 batch = []
-num = 4
-for i in range(1, num+1):
+names = []
+for i in sys.argv[1:]:
     ss = time.time()
     try:
-        x=Image.open('/home/orionx/data/imageHash/obrazkyZeSerpu/data2/' + str(i))
+        x=Image.open(i.strip())
     except:
         continue
 
@@ -21,6 +38,7 @@ for i in range(1, num+1):
     d=(d/255.0).flatten()
     d = d.tolist()
     batch.append(d)
+    names.append(i)
 
     print "Preparation:", time.time() - ss
 
@@ -28,16 +46,18 @@ for i in range(1, num+1):
 
     print i, z[:10] 
 
+if len(batch) > 0:
+    batchX = n.array(batch).transpose().flatten().tolist()
+    
+    print len(batchX)
+    #print batchX
+    
+    bb=instance.transformBatch(batchX)
+    bb = list(bb)
+    
+    batchY = n.array(bb).reshape(instance.outputNum, len(bb)/instance.outputNum).transpose()
+    
+    for i, name in enumerate(names):
+        print name, batchY[i, :10]
 
-batchX = n.array(batch).transpose().flatten().tolist()
 
-print len(batchX)
-#print batchX
-
-bb=instance.transformBatch(batchX)
-bb = list(bb)
-
-batchY = n.array(bb).reshape(instance.outputNum, len(bb)/instance.outputNum).transpose()
-
-for i in range(0, num):
-    print i, batchY[i, :10]
